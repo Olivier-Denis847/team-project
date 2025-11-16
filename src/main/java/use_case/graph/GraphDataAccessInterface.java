766335bfd56011a900fe0f567a89ac022ac5e32d
Colour1205 <me@uaena.io>
2025-11-15 22:01:44 -0500

    impleted graph dai

---
+++ src/main/java/use_case/graph/GraphDataAccessInterface.java
@@ -0,0 +1,59 @@
+package use_case.graph;
+
+import java.util.List;
+import java.util.Date;
+
+import entity.Transaction;
+
+public interface GraphDataAccessInterface {
+
+    /**
+     * get all past entries
+     * 
+     * @return all past entries in an array
+     */
+    public List<Transaction> getAllEntries();
+
+    /**
+     * save lineGraphRange data for next use
+     * 
+     * @param lineGraphRange the range data for line graph
+     */
+    public void saveGraphRange(String lineGraphRange);
+
+    /**
+     * save the type of entry to graph
+     * 
+     * @param type the type of entry
+     */
+    public void saveGraphType(String type);
+
+    /**
+     * get range data from database
+     * 
+     * @return the graph range data in database, null if file not found
+     */
+    public String getRange();
+
+    /**
+     * get type data from database
+     * 
+     * @return the pie graph range data in database, null if file not found
+     */
+    public String getType();
+
+    /**
+     * Get only expense transactions from the data source.
+     *
+     * @return list of expense transactions
+     */
+    public List<Transaction> getExpenses();
+
+    /**
+     * Get only income transactions from the data source.
+     *
+     * @return list of income transactions
+     */
+    public List<Transaction> getIncomes();
+
+}
